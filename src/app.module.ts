daf582b2b1fd48d035f38edf049546cc12c84e57
weed <rivenmle@gmail.com>
2020-12-26 12:06:42 +0700

    Fix configure bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
+import { DbConfig } from "./config/config";
 import { Module } from "@nestjs/common";
 import { UrlModule } from "./url/url.module";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/tasks", {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }),
-    UrlModule,
-  ],
+  imports: [MongooseModule.forRoot(DbConfig.url, DbConfig.options), UrlModule],
 })
 export class AppModule {}
